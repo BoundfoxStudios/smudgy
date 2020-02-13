97513b8a77d8955653266217dcb522ff59b6b3d1
Manuel Rauber <manuel.rauber@thinktecture.com>
2020-02-14 00:28:00 +0100

    fix: route to session after getting one

--- frontend/projects/smudgy/src/app/components/game/lobby/lobby.component.ts
+++ frontend/projects/smudgy/src/app/components/game/lobby/lobby.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IDebugger } from 'debug';
+import { switchMap } from 'rxjs/operators';
 import { SessionConfiguration, SessionLanguage } from '../../../models/shared/session-configuration';
+import { DebugService } from '../../../services/debug.service';
 import { SessionService } from '../../../services/session.service';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.scss'],
+  styleUrls: [ './lobby.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LobbyComponent implements OnInit {
@@ -16,20 +19,36 @@ export class LobbyComponent implements OnInit {
     roundsToPlay: 5,
   };
 
+  private readonly debug: IDebugger;
+
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly sessionService: SessionService,
+    private readonly router: Router,
+    debugService: DebugService,
   ) {
+    this.debug = debugService.derive('LobbyComponent');
   }
 
   ngOnInit(): void {
     const { sessionId } = this.activatedRoute.snapshot.queryParams;
 
     if (!sessionId) {
-      this.sessionService.createSession$(this.sessionConfiguration).subscribe();
+      this.sessionService.createSession$(this.sessionConfiguration)
+        .pipe(switchMap(serverSessionId => {
+          this.debug('Setting session id %s', serverSessionId);
+
+          return this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams: { sessionId: serverSessionId },
+            replaceUrl: true,
+          });
+        }))
+        .subscribe();
       return;
     }
 
+    console.log('jetzt sin wir hier');
     // TODO: Join existing session!
   }
 }
--- frontend/projects/smudgy/src/app/services/socket.service.ts
+++ frontend/projects/smudgy/src/app/services/socket.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { IDebugger } from 'debug';
 import { BehaviorSubject, defer, Observable } from 'rxjs';
 import { share, timeout } from 'rxjs/operators';
 import * as io from 'socket.io-client';
 import { environment } from '../../environments/environment';
 import Socket = SocketIOClient.Socket;
+import { DebugService } from './debug.service';
 
 export enum SocketState {
   Disconnected = 'disconnected',
   Connecting = 'connecting',
   Connected = 'connected'
 }
+
+let globalRequestId = 0;
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +24,10 @@ export class SocketService {
 
   private socket: Socket;
   private isInitialized: boolean;
+  private readonly debug: IDebugger;
 
-  constructor() {
+  constructor(debugService: DebugService) {
+    this.debug = debugService.derive('SocketService');
     this.socket = io(environment.gameConfiguration.backendUrl);
   }
 
@@ -57,17 +63,25 @@ export class SocketService {
 
   send$(event: string, payload: object): Observable<void> {
     return defer(() => {
+      this.debug('[Request # %d] Sending event %s with payload %o', ++globalRequestId, event, payload);
+
       this.socket.emit(event, payload);
     });
   }
 
   sendAndReceive$<T>(event: string, payload?: object): Observable<T> {
     return new Observable<T>(observer => {
+      const requestId = ++globalRequestId;
+      this.debug('[Request # %d] Sending event %s with payload %o', requestId, event, payload);
+
       this.socket.emit(event, payload, (error, result) => {
         if (error) {
+          this.debug('[Request # %d] Error %s', requestId, error);
           observer.error(error);
           return;
         }
+
+        this.debug('[Request # %d] Result %o', requestId, result);
 
         observer.next(result);
         observer.complete();
