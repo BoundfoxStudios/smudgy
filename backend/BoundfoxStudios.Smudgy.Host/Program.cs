62b5ba2589b8c1e84ab33fbda47f905ccc098a54
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2020-12-02 18:57:44 +0100

    chore: move files around

--- backend/BoundfoxStudios.Smudgy.Host/Program.cs
+++ backend/BoundfoxStudios.Smudgy.Host/Program.cs
@@ -11,7 +11,8 @@ namespace BoundfoxStudios.Smudgy.Host
     public static async Task Main(string[] args)
     {
       Log.Logger = new LoggerConfiguration()
-        .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext:l} {Message:lj}{NewLine}{Exception}")
+        .WriteTo.Console(theme: AnsiConsoleTheme.Code,
+          outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {SourceContext:l} {Message:lj}{NewLine}{Exception}")
         .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day,
           outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {SourceContext:l} {Message:lj}{NewLine}{Exception}")
         .Enrich.FromLogContext()
