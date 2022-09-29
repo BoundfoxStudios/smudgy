9f80fb8184f38bfa0c7bcc9c59e8a1be079f3e5b
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-09-29 10:46:35 +0200

    chore: update tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{html,ts}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./projects/smudgy/src/**/*.{html,ts}'],
   theme: {
     extend: {
       colors: {
