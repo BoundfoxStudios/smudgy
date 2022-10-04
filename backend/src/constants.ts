0698eac4401c1ae1a19a3f3a5f13c9dcbd5fb6f2
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-10-04 14:26:04 +0200

    feat: add possibility to override CORS_ORIGINS from environment variable

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,7 +1,11 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isDebug = !isProduction;
 
-const corsOrigins = [isDebug ? 'http://localhost:4200' : 'https://smudgy-api.onrender.com'];
+let corsOrigins = [isDebug ? 'http://localhost:4200' : 'https://smudgy-api.onrender.com'];
+
+if (process.env.CORS_ORIGINS) {
+  corsOrigins = JSON.parse(process.env.CORS_ORIGINS);
+}
 
 if (isDebug) {
   corsOrigins.push('chrome-extension://ophmdkgfcjapomjdpfobjfbihojchbko'); // Socket.io Test Client Chrome extension
